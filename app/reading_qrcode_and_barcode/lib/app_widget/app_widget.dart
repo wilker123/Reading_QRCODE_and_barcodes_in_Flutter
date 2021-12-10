2e628a5e1e14188083d5bd2fc35a3896f015964b
wilker123 <josewilkerlucena123@gmail.com>
2021-12-10 11:44:11 -0300

    Resolved error import file HomePage.dart

--- app/reading_qrcode_and_barcode/lib/app_widget/app_widget.dart
+++ app/reading_qrcode_and_barcode/lib/app_widget/app_widget.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:reading_qrcode_and_barcode/pages/home_page.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: HomePage(),
     );    
   }
