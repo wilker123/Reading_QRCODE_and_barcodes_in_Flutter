2040b54ddeb29670db5ea594d4e190e607f12289
wilker123 <josewilkerlucena123@gmail.com>
2021-12-10 11:41:02 -0300

    Created the file with HomePage! Initializing layout

--- app/reading_qrcode_and_barcode/lib/app_widget/app_widget.dart
+++ app/reading_qrcode_and_barcode/lib/app_widget/app_widget.dart
@@ -6,7 +6,7 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(),
+      home: HomePage(),
     );    
   }
 }
