4ae538c4e5e43629931306bee92d1f5467624f75
wilker123 <josewilkerlucena123@gmail.com>
2021-12-10 12:08:34 -0300

    Created theme dark for my app to read QRCode and barcodes

--- app/reading_qrcode_and_barcode/lib/app_widget/app_widget.dart
+++ app/reading_qrcode_and_barcode/lib/app_widget/app_widget.dart
@@ -6,7 +6,22 @@ class AppWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: "QRCode demo",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.grey[900],
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.tealAccent,
+            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 36),
+            onPrimary: Colors.black,
+            textStyle: TextStyle(fontSize:20, fontWeight: FontWeight.w600),
+          )
+        )
+      ),
+      themeMode: ThemeMode.dark,
       home: QRCodePage(),
     );    
   }
