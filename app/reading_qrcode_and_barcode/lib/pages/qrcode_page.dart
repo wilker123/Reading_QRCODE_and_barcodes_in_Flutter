5bf8c561c3cb90d2b61b8f7da03f9a0802125a18
wilker123 <josewilkerlucena123@gmail.com>
2021-12-10 12:22:13 -0300

    Created button to call action to read qrcode

--- app/reading_qrcode_and_barcode/lib/pages/qrcode_page.dart
+++ app/reading_qrcode_and_barcode/lib/pages/qrcode_page.dart
@@ -8,10 +8,31 @@ class QRCodePage extends StatefulWidget {
 }
 
 class _QRCodePageState extends State<QRCodePage> {
+
+  String ticket = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            if(ticket != "")
+              Padding(
+                padding: EdgeInsets.only(bottom: 24.0),
+                child: Text("Ticket: $ticket", style: TextStyle(fontSize: 20),),
+              ),
+            ElevatedButton.icon(
+              onPressed: () => {},
+              icon: Icon(Icons.qr_code),
+              label: Text("Validar"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
